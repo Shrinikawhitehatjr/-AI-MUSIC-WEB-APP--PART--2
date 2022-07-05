55beec454fd5b4be8afed0d6764a5f42a113fba5
Shrinikawhitehatjr <97172285+Shrinikawhitehatjr@users.noreply.github.com>
2022-07-05 21:29:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+song = "";
+
+function setup() {
+
+    createCanvas(600, 500);
+    canvas.center();
+}
+p5.js.preload("music.mp3").function
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
